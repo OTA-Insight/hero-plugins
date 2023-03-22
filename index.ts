b5971c640e96a8785d897d94ab5e37d09bbd0a65
brecht stamper <stamper.brecht@gmail.com>
2023-03-22 15:36:37 +0100

    test

---
+++ index.ts
@@ -0,0 +1,12 @@
+// This import statement is important for all this to work, otherwise we don't extend but replace the ulixee module definition.
+// https://github.com/microsoft/TypeScript/issues/10859
+import type {} from '@ulixee/hero/lib/extendables';
+import { IAnimations } from './lib/animations/IPlugin';
+import { ClientAnimationsPlugin, CoreAnimationsPlugin } from './lib/animations/Plugin';
+
+declare module '@ulixee/hero/lib/extendables' {
+  interface Hero extends IAnimations {}
+}
+
+export { ClientAnimationsPlugin, CoreAnimationsPlugin };
+export default { ClientAnimationsPlugin, CoreAnimationsPlugin };
